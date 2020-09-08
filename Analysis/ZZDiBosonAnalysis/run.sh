efa98da28bfb9f1f580c1969482f667bdcbc6902
vojtawar <56514436+vojtawar@users.noreply.github.com>
2020-09-08 21:54:26 +0200

    Update run.sh

--- Analysis/ZZDiBosonAnalysis/run.sh
+++ Analysis/ZZDiBosonAnalysis/run.sh
@@ -4,21 +4,8 @@ analysis="main_ZZDiBosonAnalysis"
 
 ##OPTION
 echo Which option should I run? 
-echo Options are:
-echo 0 = run all data and MC one after another
-echo 1 = run data only 
-echo 2,3 = run MC samples only 
-read varname
-echo Option is $varname
-option=$varname
-
-echo Should I use PROOF? \(will make things faster\)
-echo Options are:
-echo 0 = NO
-echo 1 = YES
-read proofvarname
-echo PROOF option is $proofvarname
-parallel=$proofvarname
+option = 1
+parallel=1
 
 
 ## execute and run ROOT
