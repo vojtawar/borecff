d551221b16665c7ee82ffbf582951ca40b11135f
vojtawar <56514436+vojtawar@users.noreply.github.com>
2020-09-17 16:28:55 +0200

    Create runpar.sh

---
+++ Analysis/ZZDiBosonAnalysis/runpar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## small script to run the analysis
+analysis="main_ZZDiBosonAnalysis"
+
+##OPTION
+option=1
+parallel=1
+
+
+## execute and run ROOT
+echo "starting ROOT"
+##
+root -l -b << EOF
+.L $analysis.C+
+$analysis($parallel,$option)
+EOF
+##
+echo "end of ROOT execution"
